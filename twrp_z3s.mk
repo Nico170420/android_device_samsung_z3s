102d189d8cc9772b8f1017d6644206b1aef2a749
Nico1704 <119746762+Nico170420@users.noreply.github.com>
2023-02-01 18:09:39 +0100

    Update twrp_z3s.mk

--- twrp_z3s.mk
+++ twrp_z3s.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/z3s/device.mk)
